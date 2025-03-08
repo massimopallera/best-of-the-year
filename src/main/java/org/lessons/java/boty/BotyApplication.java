04411fd0dc673fc788190dbee34a66bb76c5a481
massimopallera <pallera.massimo@gmail.com>
2025-03-08 09:33:35 +0100

    initial commit

---
+++ src/main/java/org/lessons/java/boty/BotyApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.boty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/boty/BotyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.boty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
